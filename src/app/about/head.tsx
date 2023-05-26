72b274306d35bbce4d7903f42b95ab994645b130
Alex Le <dunglt@innotech-vn.com>
2023-05-26 13:43:28 +0700

    test head file

---
+++ src/app/about/head.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function head() {
+  return (
+    <>
+      <title>about page</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="about page" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
